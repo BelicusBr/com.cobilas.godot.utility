e0b605239cf89dc4f6b7f50832cc481eea1ee43c
BelicusBr <x70r@outlook.com>
2024-10-17 19:35:14 -0300

    #3.2.0-rc1

--- GodotEngine.Utility/IO/GDIONull.cs
+++ GodotEngine.Utility/IO/GDIONull.cs
@@ -1,12 +1,22 @@
 namespace Cobilas.GodotEngine.Utility;
-
+/// <summary>This class is a representation of a null file.</summary>
 public sealed class GDIONull : GDFileBase {
+    /// <inheritdoc/>
     public override string Name => string.Empty;
+    /// <inheritdoc/>
     public override string NameWithoutExtension => string.Empty;
+    /// <inheritdoc/>
     public override string Path { get; protected set; } = string.Empty;
-
+    /// <inheritdoc/>
     public override GDFileBase Parent { get; protected set; } = null!;
+    /// <inheritdoc/>
     public override GDFileAttributes Attribute { get; protected set; } = GDFileAttributes.Null;
-
+    /// <summary>Represents a null file.</summary>
+    /// <returns>Returns a representation of a null file.</returns>
+    public static GDFile FileNull => new(Null, string.Empty, GDFileAttributes.Null);
+    /// <summary>Represents a null directory file.</summary>
+    /// <returns>Returns a representation of a null directory file.</returns>
+    public static GDDirectory DirectoryNull => new(Null, string.Empty, GDFileAttributes.Null);
+    /// <inheritdoc/>
     public override void Dispose() {}
 }
