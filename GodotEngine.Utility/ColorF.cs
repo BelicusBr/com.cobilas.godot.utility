6ee977818f10fade318f1cd697877bddc9fac3d2
BelicusBr <x70r@outlook.com>
2025-01-23 18:55:09 -0300

    # [4.7.0] Added Color structures such as `Color32` and `ColorF` have now been added. `Color32` represents an `rgba` value between 0 and 255. `ColorF` represents a normalized `rgba` value between 0 and 1f.

--- GodotEngine.Utility/ColorF.cs
+++ GodotEngine.Utility/ColorF.cs
@@ -4,38 +4,50 @@ using Cobilas.GodotEngine.Utility.Numerics;
 using Cobilas.GodotEngine.Utility.EditorSerialization;
 
 namespace Cobilas.GodotEngine.Utility;
+/// <summary>Represents a normalized ARGB value between (0 and 1).</summary>
 [Serializable]
 public struct ColorF : IEquatable<ColorF>, IEquatable<Vector4D> {
     [ShowRangeProperty(true, 0f, 1f)] private float r;
     [ShowRangeProperty(true, 0f, 1f)] private float g;
     [ShowRangeProperty(true, 0f, 1f)] private float b;
     [ShowRangeProperty(true, 0f, 1f)] private float a;
-
+    /// <summary>Represents the value red.</summary>
+    /// <value>Allows you to set the value red.</value>
+    /// <returns>Returns the value that represents red.</returns>
     public float R { readonly get => r; set => r = Mathf.Clamp(value, 0f, 1f); }
+    /// <summary>Represents the value green.</summary>
+    /// <value>Allows you to set the value green.</value>
+    /// <returns>Returns the value that represents green.</returns>
     public float G { readonly get => g; set => g = Mathf.Clamp(value, 0f, 1f); }
+    /// <summary>Represents the value blue.</summary>
+    /// <value>Allows you to set the value blue.</value>
+    /// <returns>Returns the value that represents blue.</returns>
     public float B { readonly get => b; set => b = Mathf.Clamp(value, 0f, 1f); }
+    /// <summary>Represents the alpha value.</summary>
+    /// <value>Allows you to set the alpha value.</value>
+    /// <returns>Returns the value that represents the alpha.</returns>
     public float A { readonly get => a; set => a = Mathf.Clamp(value, 0f, 1f); }
-
+    /// <summary>Creates a new instance of this object.</summary>
     public ColorF(float r, float g, float b, float a) {
         R = r;
         G = g;
         B = b;
         A = a;
     }
-
+    /// <summary>Creates a new instance of this object.</summary>
     public ColorF(Color32 color) : this(color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f) { }
-
+    /// <summary>Creates a new instance of this object.</summary>
     public ColorF(Color color) : this(color.r, color.g, color.b, color.a) { }
-
+    /// <summary>Creates a new instance of this object.</summary>
     public ColorF(Vector4D color) : this(color.x, color.y, color.z, color.w) { }
-
-    public bool Equals(ColorF other)
+    /// <inheritdoc/>
+    public readonly bool Equals(ColorF other)
         => other.r == r && other.g == g && other.b == b && other.a == a;
-
-    public bool Equals(Vector4D other)
+    /// <inheritdoc/>
+    public readonly bool Equals(Vector4D other)
         => other.x == r && other.y == g && other.z == b && other.w == a;
-
-    public override string ToString()
+    /// <inheritdoc/>
+    public override readonly string ToString()
         => $"(r:{r} g:{g} b:{b} a:{a})";
     /// <inheritdoc cref="Color.FromHsv(float, float, float, float)"/>
     /// <param name="h">Hue value.</param>
@@ -63,12 +75,9 @@ public struct ColorF : IEquatable<ColorF>, IEquatable<Vector4D> {
             _ => new(v, p, q, a)
         };
     }
-    /*
-    x => h
-    y => s
-    z => v
-    w => a
-    */
+    /// <summary>Converts a <seealso cref="ColorF"/> value to HSV.</summary>
+    /// <param name="c">The value to convert.</param>
+    /// <returns>Returns a <seealso cref="Vector4D"/> value with HSV values.(<c>x:h / y:s / z:v</c>)</returns>
     public static Vector4D ColorFToHSV(ColorF c) {
         float h, s, v;
         float min, max, delta;
@@ -94,8 +103,19 @@ public struct ColorF : IEquatable<ColorF>, IEquatable<Vector4D> {
 
         return new(h, s, v, c.a);
     }
-
+    /// <summary>Implicit conversion operator.(<seealso cref="Color"/> to <seealso cref="ColorF"/>)</summary>
+    /// <param name="c">Object to be converted.</param>
     public static implicit operator ColorF(Color c) => new(c);
+    /// <summary>Implicit conversion operator.(<seealso cref="Color32"/> to <seealso cref="ColorF"/>)</summary>
+    /// <param name="c">Object to be converted.</param>
     public static implicit operator ColorF(Color32 c) => new(c);
+    /// <summary>Implicit conversion operator.(<seealso cref="Vector4D"/> to <seealso cref="ColorF"/>)</summary>
+    /// <param name="c">Object to be converted.</param>
     public static implicit operator ColorF(Vector4D c) => new(c);
+    /// <summary>Explicit conversion operator.(<seealso cref="ColorF"/> to <seealso cref="string"/>)</summary>
+    /// <param name="c">Object to be converted.</param>
+    public static explicit operator string(ColorF c) => Color32.Color32ToHex(c);
+    /// <summary>Explicit conversion operator.(<seealso cref="string"/> to <seealso cref="ColorF"/>)</summary>
+    /// <param name="stg">Object to be converted.</param>
+    public static explicit operator ColorF(string stg) => Color32.HexToColor32(stg);
 }
